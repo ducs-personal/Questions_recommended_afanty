ba1c377418a323d3b9cfcef34b4cffd3ebbc98e6
李岩峰 <yanfeng.li@lejent.com>
2017-12-11 09:57:58 +0800

    查询所有年级学科下question_id相对应的knowledge_point

--- evaluation/analy_data_eval/subj_kpo_run.py
+++ evaluation/analy_data_eval/subj_kpo_run.py
@@ -13,10 +13,10 @@ from lib.table_common import getSubjKpoint
 
 _DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))).replace('\\','/')
 DATABASE = _DIR + '/new_database/Input/'
-ANALY = _DIR + '/new_database/Analy/'
+EVALUTE = _DIR + '/new_database/Analy/'
 
 PRE_DATA_PATH = DATABASE + 'Pre_data_input/'
-SUBJ_KPO_PATH = ANALY + 'Subj_Kpo_output/'
+SUBJ_KPO_PATH = EVALUTE + 'Subj_Kpo_output/'
 
 pre_province_file = 'user_json_{}_{}.txt'
 output_file = 'subj_kpo_{}_{}_{}.txt'
