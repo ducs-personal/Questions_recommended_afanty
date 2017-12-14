412daa92f8597654d940190a20e71b60d27628f0
李岩峰 <yanfeng.li@lejent.com>
2017-12-14 16:37:59 +0800

    年级学科分类，Windows下变量datetimes； Linux下 nclass_subn3 find_subkp_run.py -t 09-11 start &

--- running/pre_data_run/class_sub_run.py
+++ running/pre_data_run/class_sub_run.py
@@ -4,6 +4,8 @@ import sys
 import csv
 import logging
 import json
+import platform
+from optparse import OptionParser
 from multiprocessing import Pool
 from lib.util import getProvinceSet,mkdir
 
@@ -15,41 +17,58 @@ prov_subj_file = 'prov_subject_{}_{}_{}.txt'
 
 def packClassSub(prov, datetime, P_S_PATH):
     skp_file = 'question_sub_kpoint_{}_{}.txt'
+    exit_file = SUB_KPOINT_PATH + datetime + '/' + skp_file.format(prov, datetime)
 
-    with open(SUB_KPOINT_PATH + datetime + '/' + skp_file.format(prov, datetime), 'r') as sub_kpoint_file:
-        while True:
-            line = sub_kpoint_file.readline()
-            if line:
-                data_json = eval(line)
+    if os.path.exists(exit_file):
+        with open(exit_file, 'r') as sub_kpoint_file:
+            while True:
+                line = sub_kpoint_file.readline()
+                if line:
+                    data_json = eval(line)
 
-                sub_dic = {}
-                for i in range(1, len(data_json)):
-                    if data_json[i][1] not in sub_dic.keys():
-                        sub_dic[data_json[i][1]] = [data_json[0], data_json[i][0]]
-                    else:
-                        sub_dic[data_json[i][1]].append(data_json[i][0])
+                    sub_dic = {}
+                    for i in range(1, len(data_json)):
+                        if data_json[i][1] not in sub_dic.keys():
+                            sub_dic[data_json[i][1]] = [data_json[0], data_json[i][0]]
+                        else:
+                            sub_dic[data_json[i][1]].append(data_json[i][0])
 
-                for subj in sub_dic.keys():
-                    if len(list(sub_dic.values())[0]) > 3:
-                        with open(P_S_PATH + '/' + prov_subj_file.format(prov, subj, datetime), 'a') as new_file:
-                            new_file.writelines(json.dumps(list(sub_dic.values())[0]) + '\n')
+                    for subj in sub_dic.keys():
+                        if len(list(sub_dic.values())[0]) > 3:
+                            with open(P_S_PATH + '/' + prov_subj_file.format(prov, subj, datetime), 'a') as new_file:
+                                new_file.writelines(json.dumps(list(sub_dic.values())[0]) + '\n')
 
-            else:
-                break
+                else:
+                    break
 
-        logging.info(u"已经解析完{0}省份下{1}学科的数据，并存入到Prov_Sub_input文件！".format(prov, subj))
-        new_file.close()
-        sub_kpoint_file.close()
+            logging.info(u"已经解析完{0}省份下{1}学科的数据，并存入到Prov_Sub_input文件！".format(prov, subj))
+            new_file.close()
+            sub_kpoint_file.close()
+
+    else:
+        logging.error("该路径下{0}没有文件!".format(exit_file))
 
 
 if __name__ == '__main__':
-    datetimes = {'09-11'}
+    if 'Windows' in platform.system():
+        datetimes = {'09-11'}
+        pool = Pool(2)
+
+    elif 'Linux' in platform.system():
+        optparser = OptionParser()
+        optparser.add_option('-t', '--datetimes',
+                             dest='datetimes',
+                             help='包含时间区间的集合,若是多个时用 , （英文逗号）分割开',
+                             default='09-11')
+        (options, args) = optparser.parse_args()
+
+        datetimes = set(options.datetimes.replace(' ','').split(','))
+        pool = Pool(4)
 
     LOGGING_FORMAT = '%(asctime)-15s:%(levelname)s: %(message)s'
     prov_set = getProvinceSet()
     # prov_set = {'青海'}
     subj_set = {str(j) for j in range(1, 11)} | {str(j) for j in range(21, 31)} | {str(j) for j in range(41, 51)}
-    pool = Pool(3)
 
     for datetime in datetimes:
         logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO,
